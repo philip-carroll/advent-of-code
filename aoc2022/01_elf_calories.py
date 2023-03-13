03b44d53b22a128dbcce384dc5df51ddb06178de
Philip Carroll <philipjudecarroll@gmail.com>
2023-03-13 17:24:13 +0000

    Cleanup day 1 solution and break up into parts 1 and 2

--- aoc2022/01_elf_calories.py
+++ aoc2022/01_elf_calories.py
@@ -1,18 +1,32 @@
-with open(__file__.replace('.py', '.txt')) as f:
-    max = 0
-    running = 0
-    for line in f.readlines():
-        if line == '\n':
-            print(running)
-            if running >= max:
-                max = running
-                # print('max: %s' % max)
-            running = 0
-        else:
-            running += int(line)
+def get_calories():
+    calories = []
+
+    with open(__file__.replace('.py', '.txt')) as f:
+        subtotal = 0
+        for line in f.readlines():
+            if line == '\n':
+                calories.append(subtotal)
+                subtotal = 0
+            else:
+                subtotal += int(line)
 
     # account for last line
-    # print('running: %s' % running)
-    if running >= max:
-        max = running
-        # print('max: %s' % max)
+    calories.append(subtotal)
+
+    return calories
+
+
+def one(calories):
+    return max(calories)
+
+
+def two(calories):
+    # return the 3 largest values
+    return sum(sorted(calories)[-3:])
+
+
+if __name__ == '__main__':
+    calories = get_calories()
+
+    print(one(calories))
+    print(two(calories))
