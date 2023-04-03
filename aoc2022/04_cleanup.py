1c02921be05ab2025e92e1741ccd567af03d18d3
Philip Carroll <philipjudecarroll@gmail.com>
2023-04-03 18:20:04 +0100

    Clean up day 4 solution and remove duplicated code

--- aoc2022/04_cleanup.py
+++ aoc2022/04_cleanup.py
@@ -1,46 +1,42 @@
-def one():
-    score = 0
+def get_assignments():
+    assignments = []
 
     with open(__file__.replace('.py', '.txt')) as f:
         for line in f.readlines():
             line = line.rstrip()
 
-            e1, e2 = line.split(',')
-            e10, e11 = e1.split('-')
-            e20, e21 = e2.split('-')
-            e1r = set(range(int(e10), int(e11) + 1))
-            e2r = set(range(int(e20), int(e21) + 1))
+            range1, range2 = line.split(',')
+            range1start, range1end = range1.split('-')
+            range2start, range2end = range2.split('-')
+            range1 = set(range(int(range1start), int(range1end) + 1))
+            range2 = set(range(int(range2start), int(range2end) + 1))
+            assignments.append((range1, range2))
 
-            if e1r.issubset(e2r) or e2r.issubset(e1r):
-                score += 1
-                print(e1r)
-                print(e2r)
+    return assignments
+
+
+def one(assignments):
+    score = 0
+
+    for range1, range2 in assignments:
+        if range1.issubset(range2) or range2.issubset(range1):
+            score += 1
 
     return score
 
 
-def two():
+def two(assignments):
     score = 0
 
-    with open(__file__.replace('.py', '.txt')) as f:
-        for line in f.readlines():
-            line = line.rstrip()
-
-            e1, e2 = line.split(',')
-            e10, e11 = e1.split('-')
-            e20, e21 = e2.split('-')
-            e1r = set(range(int(e10), int(e11) + 1))
-            e2r = set(range(int(e20), int(e21) + 1))
-
-            if e1r.intersection(e2r):
-                score += 1
-                print(e1r)
-                print(e2r)
-                print(e1r.intersection(e2r))
+    for range1, range2 in assignments:
+        if range1.intersection(range2):
+            score += 1
 
     return score
 
 
 if __name__ == '__main__':
-    # print(one())
-    print(two())
+    assignments = get_assignments()
+
+    print(one(assignments))
+    print(two(assignments))
