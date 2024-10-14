aca8c9aa9d2f7525962757945f429bd7631e9053
Philip Carroll <philipjudecarroll@gmail.com>
2024-10-14 16:16:46 +0100

    Remove obsolete graph utils file

--- utils/graph_utils.py
+++
@@ -1,7 +0,0 @@
-import networkx as nx
-
-if __name__ == '__main__':
-    G = nx.Graph([(0, 1), (0, 2), (1, 2)])
-    # nx.find_cycle(G, orientation="original")
-
-    print(nx.find_cycle(G, orientation="ignore"))
